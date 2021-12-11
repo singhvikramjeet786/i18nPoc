94317c9c3784f3ce7f2273e717187528f61df8a4
Vikramjeet Singh <singhvikramjeet786@gmail.com>
2021-12-12 03:39:14 +0530

    added English as default language

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
   
   siteLanguage: string | undefined = 'English';
-  siteLocale: string = '';
+  siteLocale: string = 'en';
   languageList = [
     { code: 'en', label: 'English' },
     { code: 'fr', label: 'Français' },
@@ -19,7 +19,7 @@ export class NavBarComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("window.location :", window.location.pathname, window.location);
-    this.siteLocale = window.location.pathname.split('/')[1];
+    this.siteLocale = window.location.pathname.split('/')[1] ? window.location.pathname.split('/')[1] : 'en';
     this.siteLanguage = this.languageList.find(lang=>lang.code===this.siteLocale)?.label;
   }
 
