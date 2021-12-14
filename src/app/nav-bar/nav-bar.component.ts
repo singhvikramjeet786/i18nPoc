ac123c2cd999d6a85fb12a65598df422d71d9b30
Vikramjeet Singh <singhvikramjeet786@gmail.com>
2021-12-15 01:19:38 +0530

    refactor the nav-bar links

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -19,7 +19,7 @@ export class NavBarComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("window.location :", window.location.pathname, window.location);
-    this.siteLocale = window.location.pathname.split('/')[1]; //? window.location.pathname.split('/')[1] : 'en';
+    this.siteLocale = window.location.pathname.split('/')[1]? window.location.pathname.split('/')[1] : 'en';
     this.siteLanguage = this.languageList.find(lang=>lang.code===this.siteLocale)?.label;
   }
 
